14ce4578d485364e153319046e2f3707e4498e40
Craig Gorton <ncraiggorton@gmail.com>
2017-10-05 20:56:25 -0400

    Move satellite coms into separate file

--- src/communication.ino
+++ src/communication.ino
@@ -5,19 +5,6 @@
 //  1 (std)- mastPositionA(min-max),timeA(minutes),mastPositionB(min-max),timeB(minutes),loggingInterval(min),z (end)
 //  2 (test)- timePerTack (loggingInterval), z (end)
 
-static void setUpSat() {
-        Serial.print(F("Charging super-capacitor. Waiting "));
-        Serial.print(satChargeTime);
-        Serial.println(F(" seconds..."));
-        delay(delayForSerial);
-        gortoNap(satChargeTime); // allow capacitor to charge
-        isbd.attachConsole(Serial); // lets me see what the sat modem is doing
-        isbd.setPowerProfile(1); // low power profile
-        isbd.adjustSendReceiveTimeout(isbdTimeout);
-        isbd.setMinimumSignalQuality(3); // default is 2, trying this for stability
-        isbd.useMSSTMWorkaround(false); // I think I need this here, which is a good thing
-}
-
 static void  clearFRAM() {
         if (!fram.begin()) { // you can stick the new i2c addr in here, e.g. begin(0x51);
                 Serial.println(F("No I2C FRAM found ... check your connections."));
@@ -28,7 +15,6 @@ static void  clearFRAM() {
                         Serial.print(F("."));
                 }
         }
-        Serial.println("Done.");
 }
 
 static void useFram() {
@@ -198,40 +184,6 @@ String formattedDateNumber(byte number) {
         return tempString;
 }
 
-static void useSat() {
-        satOn();
-        do {
-                char rxBuffer[27]; // max size of rx buffer, was 16
-                // expects type(max 1 char),tackAsailPosition(max 3 chars),
-                // tackAtime(max 4 chars),tackBsailPosition(max 3 chars),
-                // tackBtime(max 4 chars),loggingInterval(maz 4 chars),z
-                // = max of 26
-                // e.g., "1,359,1439,359,1439,1439,z" is biggest message
-                size_t rxBufferSize = sizeof(rxBuffer);
-                int error = isbd.sendReceiveSBDText(txBuffer, (uint8_t *)rxBuffer, rxBufferSize);
-                if (error != 0) { // there was a problem with send/receive
-                        Serial.print(F("sendReceiveSBDText failed: error "));
-                        Serial.println(error);
-                        txSuccess = false;
-                        satOff();
-                        return;
-                }
-                txSuccess = true;
-                Serial.print(F("rxBufferSize = "));
-                Serial.println(rxBufferSize);
-                if (rxBufferSize == 0) {
-                        Serial.println(F("No message received"));
-                }
-                else {
-                        Serial.print(F("Message received: "));
-                        Serial.println(rxBuffer);
-                        parseRxBuffer(rxBuffer);
-                        Serial.print(F("Messages left: "));
-                        Serial.println(isbd.getWaitingMessageCount());
-                }
-        } while (isbd.getWaitingMessageCount() > 0);
-        satOff();
-}
 
 static void parseRxBuffer(String rxBufferAsString) {
         rxMessageInvalid = false;
@@ -311,18 +263,6 @@ static void parseRxBuffer(String rxBufferAsString) {
         Serial.println(F(" minutes"));
 }
 
-static void satOff() {
-        isbd.sleep();
-        satSS.end();
-        blinkMessage(2); // makes sure led doesn't get left ON by mistake
-        Serial.println(F("Sat off."));
-}
-
-static void satOn() {
-        Serial.println(F("Sat on."));
-        satSS.begin(satBaud);
-        isbd.begin();
-}
 
 static void blinkMessage(int condition) {
         switch (condition) {
